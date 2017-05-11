9a85e7ef915ec39ad3f2c4e81f609dcfebcd8643
Alan Monger <haveacigaro@gmail.com>
2017-05-12 00:16:37 +0100

    Add runtime cache

--- src/Handler/FixerIO.php
+++ src/Handler/FixerIO.php
@@ -6,6 +6,8 @@ class FixerIO implements CurrencyInterface
 {
     const URL = 'http://api.fixer.io/latest?base=%s';
 
+    private $rates = [];
+
     public function execute($value, $from, $to)
     {
         return $this->getRate($from, $to) * $value;
@@ -13,15 +15,18 @@ class FixerIO implements CurrencyInterface
 
     private function getRate($from, $to)
     {
-        list($base, $date, $rates) = array_values(json_decode(file_get_contents($this->buildURL($from)), true));
+        if (empty($this->rates)) {
+            list($base, $date, $rates) = array_values(json_decode(file_get_contents($this->buildURL($from)), true));
+            $this->rates = $rates;
+        }
 
         $to = strtoupper($to);
 
-        if (!array_key_exists($to, $rates)) {
+        if (!array_key_exists($to, $this->rates)) {
             throw new \DomainException("$to is not a supported currency format");
         }
 
-        return $rates[$to];
+        return $this->rates[$to];
     }
 
     private function buildURL($from)
